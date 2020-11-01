06c6978e2fcd6a8f90764e7984729dc871c8eb2b
Geoffrey Delval <geoffrey.dlvl@gmail.com>
2020-11-01 16:26:46 +0100

    Stick player to moving platform
    Resolves #47

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 using UnityEngine.Scripting.APIUpdating;
 
 public class MovingPlatform : MonoBehaviour
@@ -16,7 +17,29 @@ public class MovingPlatform : MonoBehaviour
 
     void Update()
     {
-        Move();   
+        Move();
+
+        UnparentPlayerIfRequired();
+    }
+
+    private void UnparentPlayerIfRequired()
+    {
+        PlayerController2D playerController = GetComponentInChildren<PlayerController2D>();
+        if (playerController == null)
+            return;
+
+        Transform playerTransform = playerController.transform;
+
+        RaycastHit2D hit = Physics2D.BoxCast(playerTransform.position, new Vector2(1,1), 0f, Vector2.down);
+
+        if (hit.distance > .2f)
+        {
+            playerTransform.SetParent(null);
+            foreach(Transform child in transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
     }
 
     private void Move()
@@ -27,7 +50,6 @@ public class MovingPlatform : MonoBehaviour
             var movementThisFrame = speed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
 
-            print(Vector2.Distance(transform.position, targetPosition));
             if (Vector2.Distance(transform.position, targetPosition) < 0.1f)
             {
                 waypointIndex++;
@@ -38,4 +60,16 @@ public class MovingPlatform : MonoBehaviour
             waypointIndex = 0;
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (transform.childCount == 0)
+        {
+            GameObject Empty_1x1 = new GameObject();
+            Empty_1x1.name = "Empty_1x1";
+            Empty_1x1.transform.parent = transform;
+            other.collider.transform.SetParent(Empty_1x1.transform);
+        }
+    }
+
 }
