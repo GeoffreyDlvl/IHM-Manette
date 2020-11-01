ef10f341db0d62d527f1286ce3cfe13197670cc6
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-11-01 03:58:08 +0100

    set transform parent when plyer is on moving plateform

--- Assets/Scripts/MovingPlatefrom.cs
+++ Assets/Scripts/MovingPlatefrom.cs
@@ -6,14 +6,11 @@ using UnityEngine.UIElements;
 
 public class MovingPlatefrom : MonoBehaviour
 {
-    [SerializeField]
-    private float maxMovingDistance = 5;
-    
-    [SerializeField]
-    private float a;
+    [SerializeField, Tooltip("Maximum moving range according x axis,  Ex: 5, plateform will fo from -5 to 5")]
+    private float maxMovingRange = 5;
 
-    [SerializeField]
-    private float b;
+    [SerializeField, Tooltip("Maximum speed in u/s.")]
+    private float speed = 1;
 
     private Vector3 initialTransform;
 
@@ -47,16 +44,26 @@ public class MovingPlatefrom : MonoBehaviour
     
     private void Move()
     {
-        transform.position = new Vector3(transform.position.x + ((int)currentDirection * Time.deltaTime), transform.position.y);
+        transform.position = new Vector3(transform.position.x + ((int)currentDirection * speed *Time.deltaTime), transform.position.y);
     } 
 
     private Boolean IsOutOfUpperBound()
     {
-        return gameObject.transform.position.x >= initialTransform.x + maxMovingDistance;
+        return gameObject.transform.position.x >= initialTransform.x + speed;
     }
 
     private Boolean IsOutOfLowerBound()
     {
-        return gameObject.transform.position.x <= initialTransform.x - maxMovingDistance;
+        return gameObject.transform.position.x <= initialTransform.x - speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        collision.gameObject.transform.SetParent(transform);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collision.gameObject.transform.parent = null;
     }
 }
