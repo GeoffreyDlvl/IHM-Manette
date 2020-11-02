c09df0d763fea4bc8908f8d0042f45c308473692
Geoffrey Delval <geoffrey.dlvl@gmail.com>
2020-11-02 19:05:12 +0100

    Implement level 1

---
+++ Assets/Scripts/LevelEnd.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(FindObjectOfType<PlayerController2D>().gameObject);
+        FindObjectOfType<LevelManager>().LoadNextScene();
+    }
+}
