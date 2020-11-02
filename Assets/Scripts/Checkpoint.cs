f4c39e6efd43b4332e41f018387f49e4f6ab435c
Geoffrey Delval <geoffrey.dlvl@gmail.com>
2020-11-02 12:19:08 +0100

    Create Respawn item
    Resolves #45

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    RespawnManager respawnManager;
+
+    private void Awake()
+    {
+        respawnManager = FindObjectOfType<RespawnManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        UpdateLightRenderer();
+        UpdateRespawnPosition();
+    }
+
+    private void UpdateRespawnPosition()
+    {
+        respawnManager.UpdateRespawnPosition(transform);
+    }
+
+    private void UpdateLightRenderer()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.name.Equals("Light"))
+            {
+                child.GetComponent<SpriteRenderer>().color = Color.green;
+            }
+        }    
+    }
+}
